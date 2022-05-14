85aaa4934da1575ba15c1a57295bb3c3de21077b
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-05-14 18:50:57 -0400

    put the cards together

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ import Questions from "./data/questions.json"
 
 function App() {
   return (
-  <>
-   {Questions.map(question =>{
-     return(question.q + " " + question.answer + " ")})}
-  </>
+    <div class="container">
+      <div class="quiz quiz-container">
+      <div class="quiz quiz-header"><h1>Quiz Game</h1></div>
+    <div class="quiz-allcards">
+   {Questions && Questions.map(question=>{
+      return(      
+        <div class="quiz quiz-onecard" key={question.id}>
+          <div class="quiz quiz-question">What is your {question.q}?</div>
+          <div class="answer-wrapper">
+              <div class="quiz quiz-answer">{question.a}</div>
+              <div class="quiz quiz-answer">{question.b}</div>
+              <div class="quiz quiz-answer">{question.c}</div>
+              <div class="quiz quiz-answer">{question.d}</div>
+          </div>
+          <div class="quiz quiz-submit">Submit</div>
+        </div>
+     )})}
+     </div>
+     </div>
+     </div>
   );
 }
 
