002d8300f932db4776ff05f9817a7f8a2105b36f
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-05-16 16:44:19 -0400

    added h3

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ function App() {
       <div class="quiz quiz-header"><h1>Quiz Game</h1></div>
     <div class="quiz-allcards">
    {Questions && Questions.map(question=>{
-      return(      
+      return(     
         <div class="quiz quiz-onecard" key={question.id}>
           <div class="quiz quiz-question">What is your {question.q}?</div>
           <div class="answer-wrapper">
+              <br />
               <div class="quiz quiz-answer">a. {question.a}</div>
               <div class="quiz quiz-answer">b. {question.b}</div>
               <div class="quiz quiz-answer">c. {question.c}</div>
